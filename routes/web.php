6001fdef528ae9f31b6339efd7dca8ad67c954dc
Dev-Lhabib <elhabibmahfoud@gmail.com>
2026-08-09 13:07:08 +0100

    chore: remove Laravel starter page and icons

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::redirect('/', '/login')->name('home');
 Route::inertia('/login', 'auth/login')->name('login');
 Route::inertia('/dashboard/admin', 'dashboard/admin')->name('dashboard.admin');
 Route::inertia('/dashboard/enseignant', 'dashboard/enseignant')->name('dashboard.enseignant');
